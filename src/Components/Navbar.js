069f96f3f7782598ee392cb7c168c723d36701ea
sasheed <e19271@eng.pdn.ac.lk>
2024-02-29 23:46:40 +0530

    edit navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import Logo from '../Assets/Logo.svg';
+import React, { useState } from 'react';
+import { BsCart } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import BrushIcon from '@mui/icons-material/Brush';
+import ChatIcon from '@mui/icons-material/Chat';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Testimonials",
+      icon: < ChatIcon />,
+    },
+    {
+      text: "Contact",
+      icon: < PhoneAndroidIcon />,
+    },
+    {
+      text: "Cart",
+      icon: < ShoppingCartIcon />,
+    },
+  ];
+  
+
+
+
+
   return (
     <nav>
-        <div>
-          <img src={Logo} alt="Your Company Logo" />
-        </div>
+      <div></div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Testimonial</a>
+        <a href="">Contact</a>
+        <a href="">
+          <BsCart classname="navbar-cart-icon"/>
+        </a>
+        <button className="primary-button">Hire Now</button>
+      </div>
+
+      <div className="navbar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+
+
+
+      
     </nav>
+    
     
   )
 }
