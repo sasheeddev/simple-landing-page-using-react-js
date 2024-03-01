9179ff26c9ce159a6c49342924679339f5e4211a
sasheed <e19271@eng.pdn.ac.lk>
2024-03-01 22:40:01 +0530

    finish home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Navbar from './Navbar';
+import banerbackground from '../Assets/Home-page-banner.png';
+import cover_logo from '../Assets/cover.png' ;
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-container'>
       <Navbar/>
+      <div className='home-banner-container'>
+        <div className='home-bannerImage-container'>
+          <img src={banerbackground} alt=''/>
+        </div>
+
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            We make your online presence
+          </h1>
+          <p className="primary-text">
+            Our talented team do all desiging , developing of your product
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={cover_logo} alt="" />
+        </div> 
+
+      </div>
 
  
 
