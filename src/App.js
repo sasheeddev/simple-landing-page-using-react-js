799a348889998e65b375c517dec5f728144d23f5
sasheed <e19271@eng.pdn.ac.lk>
2024-03-02 03:03:14 +0530

    built footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './Components/About';
 import Work from './Components/Work';
 import Testimonial from './Components/Testimonial';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Work/>
       <Testimonial/>
       <Contact/>
+      <Footer/>
      
       
       
